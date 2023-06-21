2142ad2f8a57b605fa2d78a47cbd3017c26979c1
samir <51479961+samir-sayyed@users.noreply.github.com>
2023-06-21 22:31:04 +0530

    Update AppConstants.kt
    removed api key

--- app/src/main/java/com/assignment/meow/utils/AppConstants.kt
+++ app/src/main/java/com/assignment/meow/utils/AppConstants.kt
@@ -1,6 +1,6 @@
 package com.assignment.meow.utils
 
-const val API_KEY = "live_y9FJerBB3q8m46VCKB8VQsjZBc6DxmOfR7yhYS7GhzRBBvtnr4YZ2dHPLIIJcSqA"
+const val API_KEY = "place your api key here"
 
 const val EMPTY_STRING = ""
 const val CAT_LIST_END_POINT = "/v1/images/search"
